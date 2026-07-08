b809d513c4f70a87a09c44d169a12372dcc79773
Julia <julia_ferreira3@estudante.sesisenai.org.br>
2026-07-08 08:06:42 -0300

    add - cpf mascarado

---
+++ ex_03.php
@@ -0,0 +1,16 @@
+<?php
+function mascararCpf($cpf)
+{
+    if (strlen($cpf) < 4) {
+        return "CPF inválido.";
+    }
+
+    $cpfMascarado = str_repeat("*", strlen($cpf) - 4) . substr($cpf, -4);
+    return $cpfMascarado;
+}
+
+$cpf = "13888138906";
+
+echo "CPF Original: $cpf <br><br>";
+echo "CPF Mascarado: " . mascararCpf($cpf);
+?>
