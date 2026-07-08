aa2947feedd72fbe1ef092bef3dfaf3ea7052087
Julia <julia_ferreira3@estudante.sesisenai.org.br>
2026-07-08 07:51:06 -0300

    up - exercicio 1 baseado no do professor

---
+++ ex_01.php
@@ -0,0 +1,20 @@
+<?php
+// (x² + y²) / (x + y)
+
+function calculateFormula($x, $y)
+{
+    if (($x + $y) == 0) {
+        return "Não é possivel realizar a divisão por zero.";
+    }
+
+    $resultado = (pow($x, 2) + pow($y, 2)) / ($x + $y);
+
+    return $resultado;
+}
+
+$x = 10;
+$y = 5;
+
+echo "Valor de x: " . $x . "<br>";
+echo "Valor de y: " . $y . "<br>";
+echo "Resultado da fórmula: " . calculateFormula($x, $y);
